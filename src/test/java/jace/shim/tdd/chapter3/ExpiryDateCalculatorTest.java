afd57660b676cc1c3fd7e762941dfd4eb5aad4c9
jaceshim <jace.shim@navercorp.com>
2020-07-17 22:02:31 +0900

    [chapter3] 만원을 납부하면 한달 뒤가 만료일자가 된다

---
+++ src/test/java/jace/shim/tdd/chapter3/ExpiryDateCalculatorTest.java
@@ -0,0 +1,20 @@
+package jace.shim.tdd.chapter3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+public class ExpiryDateCalculatorTest {
+	@Test
+	void 만원_납부하면_한달_뒤가_만료일이_된다() {
+		LocalDate billingDate = LocalDate.of(2019, 3, 1);
+		int payAmount = 10_000;
+
+		ExpiryDateCalculator expiryDateCalculator = new ExpiryDateCalculator();
+		LocalDate expiryDate = expiryDateCalculator.calculateExpiryDate(billingDate, payAmount);
+
+		assertEquals(LocalDate.of(2019, 4, 1), expiryDate);
+	}
+}
