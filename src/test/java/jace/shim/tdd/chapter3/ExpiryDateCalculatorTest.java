7db4f292af40883d34d59b359e5a0a8b586b2e9d
jaceshim <jace.shim@navercorp.com>
2020-07-17 22:06:09 +0900

    [chapter3] 만료일자 검증 케이스를 하나 추가하고 이 케이스를 통과시키기 위해 calculateExpiryDate 계산을 일반화 했다

--- src/test/java/jace/shim/tdd/chapter3/ExpiryDateCalculatorTest.java
+++ src/test/java/jace/shim/tdd/chapter3/ExpiryDateCalculatorTest.java
@@ -16,5 +16,13 @@ public class ExpiryDateCalculatorTest {
 		LocalDate expiryDate = expiryDateCalculator.calculateExpiryDate(billingDate, payAmount);
 
 		assertEquals(LocalDate.of(2019, 4, 1), expiryDate);
+
+		LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
+		int payAmount2 = 10_000;
+
+		ExpiryDateCalculator expiryDateCalculator2 = new ExpiryDateCalculator();
+		LocalDate expiryDate2 = expiryDateCalculator.calculateExpiryDate(billingDate2, payAmount2);
+
+		assertEquals(LocalDate.of(2019, 6, 5), expiryDate2);
 	}
 }
