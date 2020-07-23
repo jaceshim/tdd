988ce61fa751daa8eb0d1f1d03242a851fa59154
jaceshim <jace.shim@navercorp.com>
2020-07-23 21:29:15 +0900

    신규 calculateExpiryDate(payDate) 기반으로 리팩토링

--- src/test/java/jace/shim/tdd/chapter3/ExpiryDateCalculator.java
+++ src/test/java/jace/shim/tdd/chapter3/ExpiryDateCalculator.java
@@ -3,12 +3,6 @@ package jace.shim.tdd.chapter3;
 import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
-	public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
-		final PayData payData = PayData.builder().billingDate(billingDate).payAmount(payAmount).build();
-
-		return calculateExpiryDate(payData);
-	}
-
 	public LocalDate calculateExpiryDate(PayData payData) {
 		return payData.getBillingDate().plusMonths(1);
 	}
--- src/test/java/jace/shim/tdd/chapter3/ExpiryDateCalculatorTest.java
+++ src/test/java/jace/shim/tdd/chapter3/ExpiryDateCalculatorTest.java
@@ -9,18 +9,19 @@ import org.junit.jupiter.api.Test;
 public class ExpiryDateCalculatorTest {
 	@Test
 	void 만원_납부하면_한달_뒤가_만료일이_된다() {
-		assertExpiryDate(LocalDate.of(2019, 3, 1), 10_000, LocalDate.of(2019,4 ,1));
-		assertExpiryDate(LocalDate.of(2019, 5, 5), 10_000, LocalDate.of(2019,6 ,5));
+		assertExpiryDate(PayData.builder().billingDate(LocalDate.of(2019, 3, 1)).payAmount(10_000).build(), LocalDate.of(2019,4 ,1));
+
+		assertExpiryDate(PayData.builder().billingDate(LocalDate.of(2019, 5, 5)).payAmount(10_000).build(), LocalDate.of(2019,6 ,5));
 	}
 
 	@Test
 	void 납부일과_한달_뒤_일자가_같지_않음() {
-		assertExpiryDate(LocalDate.of(2019, 1, 31), 10_000, LocalDate.of(2019, 2, 28));
+		assertExpiryDate(PayData.builder().billingDate(LocalDate.of(2019, 1, 31)).payAmount(10_000).build(), LocalDate.of(2019, 2, 28));
 	}
 
-	private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectDate) {
+	private void assertExpiryDate(PayData payData, LocalDate expectDate) {
 		ExpiryDateCalculator expiryDateCalculator = new ExpiryDateCalculator();
-		LocalDate expiryDate = expiryDateCalculator.calculateExpiryDate(billingDate, payAmount);
+		LocalDate expiryDate = expiryDateCalculator.calculateExpiryDate(payData);
 
 		assertEquals(expectDate, expiryDate);
 	}
