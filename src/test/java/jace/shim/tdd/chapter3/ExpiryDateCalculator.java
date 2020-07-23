e3c223bd0faac80fa58e9258096f09213945d3c1
jaceshim <jace.shim@navercorp.com>
2020-07-23 21:51:13 +0900

    납부시 만료일자 계산을 위한 숫자 1을 변수로 리팩토링

--- src/test/java/jace/shim/tdd/chapter3/ExpiryDateCalculator.java
+++ src/test/java/jace/shim/tdd/chapter3/ExpiryDateCalculator.java
@@ -4,16 +4,14 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
+        int addedMonths = 1;
         if (payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
             if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
             }
         }
 
-//        if (payData.getFirstBillingDate() != null && payData.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
-//            return LocalDate.of(2019, 3, 31);
-//        }
-        return payData.getBillingDate().plusMonths(1);
+        return payData.getBillingDate().plusMonths(addedMonths);
     }
 }
